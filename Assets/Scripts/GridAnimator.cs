60e37d7c04b2d7fe3057b6db9640e0b330ccedd3
Jim Martin <jim@bitsworth.com>
2018-05-28 15:45:23 -0500

    Add some texture animation to fake forward motion

---
+++ Assets/Scripts/GridAnimator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridAnimator : MonoBehaviour {
+    [SerializeField] private float scrollSpeedX = 0.0f;
+    [SerializeField] private float scrollSpeedY = 0.0f;
+	
+	private void LateUpdate() {
+        float offsetX = Time.time * scrollSpeedX;
+        float offsetY = Time.time * scrollSpeedY;
+
+        Vector2 offset = new Vector2(offsetX, offsetY);
+
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", offset);
+	}
+}
